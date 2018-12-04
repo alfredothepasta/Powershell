9c1549a7ea329469777ec9b8a9acf0db38187575
alfredothepasta <45605623+alfredothepasta@users.noreply.github.com>
2018-12-04 12:43:09 -0700

    Execute remote commands on a list of hosts
    syntax:
    invoke-bulk -computerListPath <# path to csv or txt file containing host names #> -script { <# Script you want to run on remote hosts #> }

---
+++ invoke-bulk.ps1
@@ -0,0 +1,41 @@
+function invoke-bulk {
+
+    param(
+        [parameter(Mandatory)]
+        [string]$computerListPath,
+        [parameter(Mandatory,ValueFromPipeline)]
+        [System.Management.Automation.ScriptBlock]$script
+        )
+    Begin {    
+        $computername = get-content -path $computerListPath
+        $conFail = @()
+    }
+
+    Process {
+
+        foreach ($computer in $computername) {
+            
+            $r = Test-WSMan $Computer -ErrorAction SilentlyContinue
+
+
+            if ($r){
+            
+                
+
+                invoke-command -computername $computer -credential $cred -scriptblock $script
+            } else {
+                
+                $conFail += $computer 
+
+            }            
+        }
+    }
+    
+    End {
+        
+        # 
+        $confail = $conFail | out-string -stream
+        $confail | convertto-html | out-file -FilePath C:\psstuff\ConnectionTest.csv
+        
+    }
+}
