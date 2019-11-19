21a046947dbf92f3fcc7b15cb829410549a5d36a
Alex Reed <Alex.Reed@amgh.us>
2019-11-18 18:22:26 -0700

    added a few more of my scripts

---
+++ FixTheGPOFuckUp.ps1
@@ -0,0 +1,17 @@
+﻿# Fix the broken
+
+
+$keyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
+$keys = get-item $keyPath | select-object -expandproperty property
+
+foreach ($item in $keys) {
+    
+    $value = (get-itemproperty "$keypath").$item
+    if($value -like("*sp-smf-fs01\home-fs03*")){
+        $value = $value -Replace("sp-smf-fs01\\home-fs03", "sp-smf-fs01\datastore-fs03\home")
+        #$value
+        Set-ItemProperty -Path $keyPath -Name $item -Value $value
+        }
+    }
+
+gpupdate /force
---
+++ RCHFolderRedirectionRegistryEdit.ps1
@@ -0,0 +1,17 @@
+﻿# Must be pasted into a non-elevated powershell window
+# Must be logged into the afflicted user's account
+
+$keyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
+$keys = get-item $keyPath | select-object -expandproperty property
+
+foreach ($item in $keys) {
+    
+    $value = (get-itemproperty "$keypath").$item
+    if($value -like("*csmc-fs03\home*")){
+        $value = $value -Replace("csmc-fs03\\home", "sp-smf-fs01\datastore-fs03\home")
+        #$value
+        Set-ItemProperty -Path $keyPath -Name $item -Value $value
+        }
+    }
+
+gpupdate /force
