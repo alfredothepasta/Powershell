07735487450f8d4cac5effeed75ffd7ffa8f2dd0
Alex Reed <Alex.Reed@amgh.us>
2019-12-06 12:44:46 -0700

    made remotingAndClearing more bulletproof

--- remotingAndClearing.ps1
+++ remotingAndClearing.ps1
@@ -2,20 +2,24 @@
 # Input the computer you want to run the script on when running, automagically
 #   remotes into the comptuer and finds all old profiles.  
 
-# Run this chunk separately
+# Run this chunk separately, this just remotes you in. 
 $cred = Get-Credential
 $computerName = read-host -prompt 'Input computer name'
 New-PSSession -ComputerName $computerName -Credential $cred | Enter-PSSession
 
+# This chunk can be run remotely or locally
+# Change this to set the threshold for how recently a user should have logged in default 1 year
+$numDays = 365 
 
-$cutoff = (get-date).addDays(-322)
-$oldUsers = (get-childitem C:\users) | where-object {$_.lastwritetime -le $cutoff -and $_.name -notlike "admin*" -and $_.name -notlike "public"} | select name
+
+$cutoff = (get-date).addDays(-$numDays)
+$oldUsers = (get-childitem C:\users) | where-object {$_.lastwritetime -le $cutoff -and $_.name -notlike "admin*" -and $_.name -notlike "public" -and $_.name -notlike "default" -and $_.name -notlike "temp" -and $_.name -notlike "guest"} | Select-Object name
 foreach($user in $oldUsers){
     $userString = $user.name
     write-output $userString
-    Get-WMIObject -class Win32_UserProfile | Where {
+    Get-WMIObject -class Win32_UserProfile | Where-Object {
         ($_.localpath -eq "C:\Users\$userString")
-    } | remove-wmiobject       
+    } | remove-wmiobject    
 }
 
 cleanmgr.exe /AUTOCLEAN
